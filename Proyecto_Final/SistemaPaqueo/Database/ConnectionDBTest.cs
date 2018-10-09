20065bab08255d2a1970b03334f9023ba9e746b0
Diegoalru <43016665+Diegoalru@users.noreply.github.com>
2018-10-09 11:58:47 -0600

    Conexion con base de datos.

---
+++ Proyecto_Final/SistemaPaqueo/Database/ConnectionDBTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Database
+{
+    public class ConnectionDBTest
+    {
+
+        private string Server;
+        private string Database;
+        private string UID;
+        private string Password;
+        public static MySqlConnection dbConn;
+
+        public ConnectionDBTest()
+        {
+            Server   = "localhost";
+            Database = "";
+            UID      = "";
+            Password = "";
+        }
+
+        public string ReturnDBConnection()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = Server;
+            builder.Database = Database;
+            builder.UserID = UID;
+            builder.Password = Password;
+
+            String connString = builder.ToString();
+            return connString;
+        }
+
+    }
+}
